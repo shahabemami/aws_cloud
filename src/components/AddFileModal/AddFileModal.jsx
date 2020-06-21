0553be615ddc27367056a1219efda6fca327b2dc
Shahab Emami <shahab.emami65@gmai.com>
2020-06-21 10:42:14 -0700

    upload multiple files

--- src/components/AddFileModal/AddFileModal.jsx
+++ src/components/AddFileModal/AddFileModal.jsx
@@ -42,22 +42,37 @@ const AddFileModal = ({ open, onClose, fetchAllFiles }) => {
 
   const { enqueueSnackbar } = useSnackbar();
 
-  const onUpload = () => {
+  const onUpload = async () => {
     if (files.length > 0) {
       setIsLoading(true);
       // upload files
-      Storage.put(files[0].name, files[0], { level: 'protected' })
-        .then((file) => {
-          enqueueSnackbar('successfully uploaded', { variant: 'success' });
-          fetchAllFiles('', 'protected');
-          setIsLoading(false);
-          onClose();
-        })
-        .catch((error) => {
-          enqueueSnackbar(error.message, { variant: 'error' });
-          setIsLoading(false);
-          onClose();
-        });
+
+      try {
+        for (const file of files) {
+          await Storage.put(file.name, file, { level: 'protected' });
+        }
+        setIsLoading(false);
+        enqueueSnackbar('successfully uploaded', { variant: 'success' });
+        fetchAllFiles('', 'protected');
+        onClose();
+      } catch (error) {
+        enqueueSnackbar(error.message, { variant: 'error' });
+        setIsLoading(false);
+        onClose();
+      }
+
+      // Storage.put(files[0].name, files[0], { level: 'protected' })
+      //   .then((file) => {
+      //     enqueueSnackbar('successfully uploaded', { variant: 'success' });
+      //     fetchAllFiles('', 'protected');
+      //     setIsLoading(false);
+      //     onClose();
+      //   })
+      //   .catch((error) => {
+      //     enqueueSnackbar(error.message, { variant: 'error' });
+      //     setIsLoading(false);
+      //     onClose();
+      //   });
     } else {
       enqueueSnackbar('you need to select at least one file', { variant: 'warning' });
     }
